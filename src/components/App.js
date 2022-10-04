22817d1d07e8fc73f4e87710d08864c228ebfdc7
Frankiline <frankiline.mwaura@student.moringaschool.com>
2022-10-04 21:00:19 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import NavBar from "./NavBar";
 import MoviesPage from "./MoviesPage";
 
@@ -13,14 +13,14 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Switch>
+      <Routes>
         <Route path="/movies">
           <MoviesPage movies={movies} />
         </Route>
         <Route exact path="/">
           <div>Home</div>
         </Route>
-      </Switch>
+      </Routes>
     </div>
   );
 }
